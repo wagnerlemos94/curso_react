9c0a9444c397a5c507dc862d710fb340d858b529
= <wagner.lemos@ses.ba>
2021-07-07 12:06:44 -0300

    Correcao de error de cors

--- src/views/Login.js
+++ src/views/Login.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Card from '../components/Card';
 import FormGroup from '../components/Form-group';
 import { withRouter } from 'react-router-dom';
+import axios from 'axios';
 
 
 class Login extends React.Component{
@@ -12,8 +13,14 @@ class Login extends React.Component{
     }
 
     entrar = () => {
-        console.log('Email: ', this.state.email)
-        console.log('Senha: ', this.state.senha)
+        axios.post('http://localhost:8080/api/usuarios/autenticar',{
+            email: this.state.email,
+            senha: this.state.senha
+        }).then(response => {
+            console.log(response);
+        }).catch(erro => {
+            console.log(erro.response);
+        });
     }
 
     prepareCadastrar = () => {
