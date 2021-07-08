4646d39bcb29f294ad0743c7613892254ca3cbaa
wagnerlemos94 <wagner.lemos94@hotmail.com>
2021-07-08 00:17:19 -0300

    recuperando usuario via sessao

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,10 @@ class Home extends React.Component{
     }
 
     componentDidMount(){
-        axios.get('http://localhost:8080/api/usuarios/4/saldo')
+        const usuarioLogadoString = localStorage.getItem('_usuario_logado');
+        const usuarioLogado = JSON.parse(usuarioLogadoString);
+        console.log('usuario logado do localStorage', usuarioLogado);
+        axios.get(`http://localhost:8080/api/usuarios/${usuarioLogado.id}/saldo`)
         .then(response => {
             this.setState({saldo:response.data});
         }).catch( erro => {
