da3155a01e9e16acee0d74b105a3ff95d8cc9522
wagnerlemos94 <wagner.lemos94@hotmail.com>
2021-07-08 01:17:06 -0300

    messagem personalizada com toastr

--- src/main/App.js
+++ src/main/App.js
@@ -3,8 +3,11 @@ import React from 'react';
 import Rotas from './Rotas';
 import NavBar from '../components/NavBar';
 
-import 'bootswatch/dist/flatly/bootstrap.css'
+import 'toastr/build/toastr.min.js';
+
+import 'bootswatch/dist/flatly/bootstrap.css';
 import '../custom.css';
+import 'toastr/build/toastr.css';
 
 class App extends React.Component {
 
