caabed93917101e6317c83c58192dd5bbcd3d011
wagnerlemos94 <wagner.lemos94@hotmail.com>
2021-07-11 20:35:16 -0300

    Service e view de lancamento

--- src/views/lancamnetos/Consulta-lancamento.js
+++ src/views/lancamnetos/Consulta-lancamento.js
@@ -5,8 +5,46 @@ import FormGroup from '../../components/Form-group';
 import SelectMenu from '../SelectMenu';
 import LancamentoTable from './LancamentoTable';
 
+import LancamentoService from '../../app/service/LancamentoService';
+import LocalStorageService from '../../app/service/localStorageService';
+
 
 class ConsultaLancamento extends React.Component{
+
+    state = {
+        ano: '',
+        mes: '',
+        tipo: '',
+        lancamentos:[]
+    }
+
+    constructor(){
+        super();
+        this.service = new LancamentoService;
+    }
+
+
+    buscar = () =>{
+        const usuarioLogado = LocalStorageService.obterItem('_usuario_logado');
+
+        
+        const lancamentoFiltro = {
+            ano: this.state.ano,
+            mes: this.state.mes,
+            tipo: this.state.tipo,
+            usuario: usuarioLogado.id
+        }
+        
+        this.service
+            .consultar(lancamentoFiltro)
+            .then(response => {
+            this.setState({lancamentos: response.data})
+            }).catch(erro => {
+                console.log(erro)
+            });
+        
+    }
+
     render(){
         const mes = [
          { label: 'Selecione..', value: ''},
@@ -42,19 +80,28 @@ class ConsultaLancamento extends React.Component{
                             <FormGroup htmlFor="inputAno" label="Ano: *">
                                 <input type="text" className="form-control" 
                                 id="exampleInputEmail1"
-                                aria-describedby="emailHelp"
+                                value={this.state.ano}
+                                onChange={e => this.setState({ano: e.target.value})}
                                 placeholder="Digite o Ano" />
                             </FormGroup>
                             
                             <FormGroup htmlFor="inputMes" label="Mes: *">
-                                <SelectMenu id="inputMes" className="form-control" lista={mes} />
+                                <SelectMenu id="inputMes"
+                                className="form-control" 
+                                value={this.state.mes}
+                                onChange={e => this.setState({mes: e.target.value})}
+                                lista={mes} />
                             </FormGroup>
 
                             <FormGroup htmlFor="inputTipo" label="Tipo Lançamento: *">
-                                <SelectMenu id="inputTipo" className="form-control" lista={tipos} />
+                                <SelectMenu id="inputTipo"
+                                className="form-control" 
+                                value={this.state.tipo}
+                                onChange={e => this.setState({tipo: e.target.value})}
+                                lista={tipos} />
                             </FormGroup>
 
-                            <button type="button" className="btn btn-success">Buscar</button>
+                            <button onClick={this.buscar} type="button" className="btn btn-success">Buscar</button>
                             <button type="button" className="btn btn-danger">Cadastrar</button>
 
                         </div>
@@ -64,7 +111,7 @@ class ConsultaLancamento extends React.Component{
                 <div className="row">
                     <div className="col-md-12">
                         <div className="bs-component">
-                            <LancamentoTable lancamentos={lancamentos}/>
+                            <LancamentoTable lancamentos={this.state.lancamentos}/>
                         </div>
                     </div>
                 </div>
